5d4f3738f40051415feb1e8a41a481c9125c67a7
Thibault Jaime <jaime.thibault@gmail.com>
2018-04-03 11:22:55 +0200

    added €/m2 calculation to reponse to API

--- connect_seloger_api.rb
+++ connect_seloger_api.rb
@@ -9,7 +9,12 @@ puts ">>>>>>>>>>>>>>"
 document.root.xpath('annonces').each do |annonce|
   annonce.xpath('annonce').each do |element|
     idAnnonce   = element.xpath('idAnnonce').text
-    puts "annonce: #{idAnnonce}"
+    surface     = element.xpath('surface').text
+    prix        = element.xpath('prix').text
+    ville       = element.xpath('ville').text
+    cp          = element.xpath('cp').text
+
+    puts "#{idAnnonce} | #{prix.to_f.fdiv(surface.to_f).round(0)}€/m2 | #{surface}m2 pour #{prix}€ à #{cp} | #{ville}"
   end
 end
 
